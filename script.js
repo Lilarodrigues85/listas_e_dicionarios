7cd9c7d739d1ec7fa676d0e2e8b1fcd8c5064d1e
Lila <dalila.analistadesistema@gmail.com>
2023-08-07 19:45:10 -0300

    ...

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
-var lista = "spiderman";
+var lista = [
+   {nome: "spiderman", ano: 2000, categoria: "aventura"},
+   {nome: "hulk", ano: 2002, categoria: "ação"},
+   {nome: "iroman", ano: 2010, categoria: "scify"}
+];
 
 document.getElementById("button").onclick = function(){
-   document.getElementById("nomes").innerHTML = lista;
+   document.getElementById("nomes").innerHTML = lista[0].ano;
 }
